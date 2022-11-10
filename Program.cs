994c03551a9fb3682965b57f95a68f092001debc
Chung Nguyen <zrmedia9@gmail.com>
2022-11-10 21:40:10 +0700

    Install EntityFramework and add connectstring, dbcontext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 global using first_api.Models;
+using first_api.Data;
 using first_api.Services.CharacterService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
