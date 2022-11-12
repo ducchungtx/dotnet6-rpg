5e0d92faaf7c27beff0f3fb6a3b24ebbcc5ed5ec
Chung Nguyen <zrmedia9@gmail.com>
2022-11-12 21:16:23 +0700

    add first migration and get implementions

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using first_api.Data;
 using first_api.Dtos.Character;
+using Microsoft.EntityFrameworkCore;
 
 namespace first_api.Services.CharacterService
 {
@@ -20,10 +22,12 @@ namespace first_api.Services.CharacterService
             },
         };
         private readonly IMapper _mapper;
+        private readonly DataContext _context;
 
-        public CharacterService(IMapper mapper)
+        public CharacterService(IMapper mapper, DataContext context)
         {
             _mapper = mapper;
+            _context = context;
         }
 
         public async Task<ServiceReponse<List<GetCharacterDto>>> AddCharacter(
@@ -59,17 +63,17 @@ namespace first_api.Services.CharacterService
 
         public async Task<ServiceReponse<List<GetCharacterDto>>> GetAllCharacters()
         {
-            return new ServiceReponse<List<GetCharacterDto>>
-            {
-                Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList()
-            };
+            var reponse = new ServiceReponse<List<GetCharacterDto>>();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            reponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            return reponse;
         }
 
         public async Task<ServiceReponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceResponse = new ServiceReponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            var dbCharacters = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacters);
             return serviceResponse;
         }
 
