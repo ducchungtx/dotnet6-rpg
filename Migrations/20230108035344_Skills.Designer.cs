df512dee818a2df27fd1ea578658236ed95d28bf
Chung Nguyen <zrmedia9@gmail.com>
2023-01-08 11:13:11 +0700

    Add skill, data seeding and many-many relationship

--- Migrations/20230108035344_Skills.Designer.cs
+++ Migrations/20230108035344_Skills.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using first_api.Data;
 
@@ -11,9 +12,11 @@ using first_api.Data;
 namespace firstapi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230108035344_Skills")]
+    partial class Skills
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -92,26 +95,6 @@ namespace firstapi.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Skills");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Damage = 30,
-                            Name = "Fireball"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            Damage = 20,
-                            Name = "Frenzy"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            Damage = 50,
-                            Name = "Blizzard"
-                        });
                 });
 
             modelBuilder.Entity("first_api.Models.User", b =>
--- Models/Character.cs
+++ Models/Character.cs
@@ -5,16 +5,17 @@ using System.Threading.Tasks;
 
 namespace first_api.Models
 {
-  public class Character
-  {
-    public int Id { get; set; }
-    public string Name { get; set; } = "Chung";
-    public int HitPoints { get; set; } = 100;
-    public int Strength { get; set; } = 10;
-    public int Defense { get; set; } = 10;
-    public int Intelligence { get; set; } = 10;
-    public RpgClass Class { get; set; } = RpgClass.Knight;
-    public User? User { get; set; }
-    public Weapon? Weapon { get; set; }
-  }
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Chung";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+        public User? User { get; set; }
+        public Weapon? Weapon { get; set; }
+        public List<Skill>? Skills { get; set; }
+    }
 }
